62abd0f6c75d043f7f89c48e2febff7991bcd302
Aeshar Manzar <aesharmanzar@gmail.com>
2022-10-29 19:10:11 +0530

    First Commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        int age = 25;
+        String name = "Aeshar Manzar";
+
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,24 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        boolean flag = true;
+
+        char ch = 'A';
+
+        byte age = 10;
+        short salary = 12500;
+        int annualIncome = 3000000;
+        long companyBudget = 500000000;
+
+        float dailyExpense = 10.11F;
+        double  monthlyExpense = 40.600;
+
+        System.out.println("flag " + flag);
+        System.out.println("character " + ch);
+        System.out.println("age " + age);
+        System.out.println("salary " + salary);
+        System.out.println("annual income" + annualIncome);
+        System.out.println("company budget" + companyBudget);
+        System.out.println("daily expense" + dailyExpense);
+        System.out.println("monthly expense" + monthlyExpense);
+    }
+}
